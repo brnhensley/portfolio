1af39d5733b8024961d5e4c1aa1bee020f826e35
Brian Hensley <brnhensley@gmail.com>
2019-06-14 09:57:05 -0700

    generate header component

--- the-onion/src/app/app.module.ts
+++ the-onion/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import 'materialize-css';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
